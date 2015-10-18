d279749bf714f9602746b8adf39a391a4f91ecaf
Joshua Wannebo <jwannebo@mail.usf.edu>
2015-10-18 13:51:15 -0400

    Parse the grid

--- Project Euler/Program.cs
+++ Project Euler/Program.cs
@@ -224,7 +224,22 @@ namespace Project_Euler
 20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
 20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
 01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48";
-            return 0;
+
+            int rows = 20, cols = 20;
+
+            int[][] grid = new int[rows][];
+            int iter = 0;
+
+            foreach (string row in gridStr.Split(Environment.NewLine[0]))
+            {
+                grid[iter++] = Array.ConvertAll(row.Split(' '), int.Parse);
+            }
+
+            int maxProduct = -1;
+
+
+
+            return maxProduct;
         }
     }
 }
